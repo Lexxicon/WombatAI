ebe567a1671b5336bdc000281a71031717f9bba0
bholman <dolorosul@gmail.com>
2017-07-24 08:59:42 -0500

    basic emergency behaviour

--- src/behaviour/emergencyMiner.ts
+++ src/behaviour/emergencyMiner.ts
@@ -1,20 +1,55 @@
 import { Behaviour } from "behaviour/behaviour";
-
+import { log } from "lib/logger/log";
 export enum State {
   HARVEST = 1,
   DROP_OFF,
 }
 export class EmergencyMiner implements Behaviour {
 
-  private updateState(creep: Creep): State {
-    switch (creep.state) {
-
+  public run(creep: Creep): void {
+    creep.memory.state = this.updateState(creep);
+    switch (creep.memory.state) {
+      case State.DROP_OFF:
+        this.dropOff(creep);
+        break;
+      case State.HARVEST:
+        this.harvest(creep);
+        break;
     }
-    return State.HARVEST;
   }
 
-  public run(creep: Creep): void {
-    creep.state = updateState(creep);
+  private dropOff(creep: Creep): void {
+    const target = creep.room.find(FIND_MY_SPAWNS)[0] as Spawn;
+    if (!target) {
+      log.warning("failed to find drop off spawn");
+      return;
+    }
+    if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+
+  private harvest(creep: Creep): void {
+    const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source;
+    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+
+  private updateState(creep: Creep): State {
+    switch (creep.memory.state) {
+      case State.DROP_OFF:
+        if (_.sum(creep.carry) === 0) {
+          return State.HARVEST;
+        }
+        break;
+      case State.HARVEST:
+        if (_.sum(creep.carry) === creep.carryCapacity) {
+          return State.DROP_OFF;
+        }
+        break;
+    }
+    return creep.memory.state;
   }
 
 }
